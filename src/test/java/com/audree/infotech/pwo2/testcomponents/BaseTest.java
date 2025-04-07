d8d73c0228ad797059917b04dbdde1890239e54d
Sharuk <sharuk.komminapalli@audreeinfotech.com>
2025-04-07 18:12:24 +0530

    Updated

--- src/test/java/com/audree/infotech/pwo2/testcomponents/BaseTest.java
+++ src/test/java/com/audree/infotech/pwo2/testcomponents/BaseTest.java
@@ -181,7 +181,7 @@ public class BaseTest {
 		js.executeScript("window.scrollTo(0, document.body.scrollHeight)");
 		Thread.sleep(2000);
 		//Added
-		System.out.println("CI/CD-integration");
+		System.out.println("CI/CD-integration-Jenkins");
 	}
 
 	// ***************************************************************************************************
