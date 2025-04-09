e6da42421a63879bf2209d209b06273b32af0723
Sharuk <sharuk.komminapalli@audreeinfotech.com>
2025-04-09 16:59:47 +0530

    Checking jenkins

--- src/test/java/com/audree/infotech/pwo2/tests/Transcations/CategorizationOfWorkTest.java
+++ src/test/java/com/audree/infotech/pwo2/tests/Transcations/CategorizationOfWorkTest.java
@@ -69,6 +69,8 @@ public class CategorizationOfWorkTest extends BaseTest {
 			categorizationOfWorkPom.okButton();
 			Thread.sleep(1000);
 			System.out.println("Record Saved Successfully");
+			System.out.println("Record updated Successfully");
+
 		} catch (Exception e) {
 			System.out.println("savingRecord method failed :" + e);
 		}
